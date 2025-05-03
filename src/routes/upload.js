1fa23a3ef1f6c462e8720055c42a485fecc9ef62
David <da3909622@gmail.com>
2025-05-02 23:26:48 -0600

    Add base

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const upload = require('../config/uploadConfig');
 const router = express.Router();
-
+const BASE_URL = process.env.BASE_URL;
 // Modificado para permitir múltiples archivos
 router.post('/', upload.array('upload', 10), (req, res) => {  // Limite de 10 imágenes, ajusta si es necesario
     if (!req.files || req.files.length === 0) {
         return res.status(400).json({ error: 'No images uploaded' });
     }
     
-    const fileUrls = req.files.map(file => `http://${process.env.HOST}:${process.env.PORT}/api/uploads/${file.filename}`);
+    const fileUrls = req.files.map(file => `${BASE_URL}/api/uploads/${file.filename}`);
 
     res.json({
         urls: fileUrls
