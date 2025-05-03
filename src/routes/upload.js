7108c19b255396420aa27a48279eadc8e63ae5bd
david69-js <da3909622@gmail.com>
2025-05-02 23:45:33 -0400

    upload conf

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -8,7 +8,7 @@ router.post('/', upload.array('upload', 10), (req, res) => {  // Limite de 10 im
         return res.status(400).json({ error: 'No images uploaded' });
     }
     
-    const fileUrls = req.files.map(file => `http://${process.env.HOST}:${process.env.PORT}/uploads/${file.filename}`);
+    const fileUrls = req.files.map(file => `http://${process.env.DB_SERVER}:${process.env.PORT}/uploads/${file.filename}`);
 
     // Responder con un array de URLs
     res.json({
